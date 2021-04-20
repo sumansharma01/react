8af9c835f6e13dda59210adc86cf8e21ce24ce9f
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-20 19:48:45 +0530

    added dynamic values

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 
 const App=()=>{
+  const title="Welcome to my new blog";
   return(
     <div className="App">
       <div className="content">
         <h1>Blog</h1>
+        <p>{title}</p>
+        <p>{Math.ceil(Math.random()*10)}</p>
       </div>
     </div>
   );
