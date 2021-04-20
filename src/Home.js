35ecdde2fae9dc785a9cbf8f8605a8d16398e3fe
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-20 21:05:04 +0530

    added events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,26 @@
 const Home = () => {
     const title="Welcome to home";
+    
+    const randomGenerator=()=>{
+        const randomm=Math.ceil(Math.random()*10);
+        const home=document.querySelector(".home");
+        const p=document.createElement("p");
+        p.innerText=randomm;
+        home.appendChild(p);
+    }
+    const passingArgument=(name)=>{
+        const home=document.querySelector(".home");
+        const p=document.createElement("p");
+        p.innerText=name;
+        home.appendChild(p);
+    }
     return (  
         <div className="home">
             <h1>{title}</h1>
-            <p>{Math.ceil(Math.random()*10)}</p>
+            <button onClick={randomGenerator}>Random</button>
+
+            <button onClick={()=>passingArgument("Suman")}>Click</button>
+
         </div>
     );
 }
