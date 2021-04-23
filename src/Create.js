f406c9a089c9d4e7a35792b506def2aba215319f
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-23 20:50:29 +0530

    added form

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,31 @@
+import {useState} from 'react';
 const Create = () => {
+    const [title,setTitle]=useState('');
+    const [author,setAuthor]=useState('');
     return ( 
         <div className="create-blog">
             <h1> Create a blog</h1>
+            <label>Title</label>
+            <input 
+                type="text"
+                value={title}
+                onChange={(e)=>{setTitle(e.target.value)}}
+            />
+            <label>Author</label>
+            <select
+                onChange={(e)=>{setAuthor(e.target.value)}}
+            >
+                <option value="suman">Suman</option>
+                <option value="pujan">Pujan</option>
+                <option value="risu">Risu</option>
+            </select>
+
+            <div>
+                <p>{title}</p>
+                <p>{author}</p>
+            </div>
         </div>
+
      );
 }
  
