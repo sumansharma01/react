dd964761dea233452eafcf4e058be5c776a4c6c6
sumansharma01 <suman.sharma0331@gmail.com>
2021-04-23 21:10:19 +0530

    redirect using useHistory

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,30 @@
 import {useState} from 'react';
+import { useHistory } from 'react-router-dom';
 const Create = () => {
     const [title,setTitle]=useState('');
     const [author,setAuthor]=useState('');
+    const history=useHistory();
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const data={title,author};
+
+        fetch("http://localhost:8000/blogs",{
+            method:'POST',
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify(data)
+        }).then(()=>{
+            console.log("new blog added");
+            history.push("/");
+
+        })
+        
+    }       
+
     return ( 
         <div className="create-blog">
             <h1> Create a blog</h1>
+            <form>
             <label>Title</label>
             <input 
                 type="text"
@@ -19,11 +39,9 @@ const Create = () => {
                 <option value="pujan">Pujan</option>
                 <option value="risu">Risu</option>
             </select>
-
-            <div>
-                <p>{title}</p>
-                <p>{author}</p>
-            </div>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+            </form>
+           
         </div>
 
      );
